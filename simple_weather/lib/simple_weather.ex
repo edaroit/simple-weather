b2f69752effaceaa41109d04ce970066d022396d
Eliseu Daroit <eliseudaroitjr@gmail.com>
2018-04-24 23:27:49 -0300

    Add simple_weather logic

--- simple_weather/lib/simple_weather.ex
+++ simple_weather/lib/simple_weather.ex
@@ -1,18 +1,49 @@
 defmodule SimpleWeather do
-  @moduledoc """
-  Documentation for SimpleWeather.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> SimpleWeather.hello
-      :world
-
-  """
-  def hello do
-    :world
+  def start(cities) do
+    cities
+    |> Enum.map(&create_task/1)
+    |> Enum.map(&Task.await/1)
   end
+
+  def get_appid(), do: "021ded034a0b847a469bc93238c95074"
+
+  def kelvin_to_celsius(kelvin) do
+    (kelvin - 273.15) |> Float.round(1)
+  end
+
+  def temperature_of(location) do
+    result = get_endpoint(location) |> HTTPoison.get() |> parser_response
+
+    case result do
+      {:ok, temp} ->
+        "#{location}: #{temp} °C"
+
+      :error ->
+        "#{location} not found"
+    end
+  end
+
+  defp create_task(city) do
+    Task.async(fn -> temperature_of(city) end)
+  end
+
+  defp compute_temperature(json) do
+    try do
+      temp = json["main"]["temp"] |> kelvin_to_celsius
+      {:ok, temp}
+    rescue
+      _ -> :error
+    end
+  end
+
+  defp get_endpoint(location) do
+    location = URI.encode(location)
+    "http://api.openweathermap.org/data/2.5/weather?q=#{location}&appid=#{get_appid()}"
+  end
+
+  defp parser_response({:ok, %HTTPoison.Response{body: body, status_code: 200}}) do
+    body |> JSON.decode!() |> compute_temperature
+  end
+
+  defp parser_response(_), do: :error
 end
--- simple_weather/test/simple_weather_test.exs
+++ simple_weather/test/simple_weather_test.exs
@@ -1,8 +1,28 @@
-defmodule SimpleWeatherTest do
-  use ExUnit.Case
-  doctest SimpleWeather
+defmodule SimpleWeather.Test do
+  use ExUnit.Case, async: true
 
-  test "greets the world" do
-    assert SimpleWeather.hello() == :world
+  describe "when receives Kelvin" do
+    test "returns Celsius" do
+      kelvin = 296.48
+      celsius = 23.3
+      temperature = SimpleWeather.kelvin_to_celsius(kelvin)
+      assert temperature == celsius
+    end
+  end
+
+  describe "when receive a valid location" do
+    test "returns temperature" do
+      location = "Porto Alegre"
+      temperature = SimpleWeather.temperature_of(location)
+      assert String.contains?(temperature, location) == true
+    end
+  end
+
+  describe "when receive an invalid location" do
+    test "returns not found" do
+      invalid_location = "00000"
+      result = SimpleWeather.temperature_of(invalid_location)
+      assert result == "#{invalid_location} not found"
+    end
   end
 end
